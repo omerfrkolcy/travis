4615f82ebff672ec3fbde92285be5e9b577004b6
omerfrkolcy <omerfrkolcy@hotmail.com>
2021-04-18 12:03:46 +0300

    mongodb transition

--- go.mod
+++ go.mod
@@ -4,14 +4,16 @@ go 1.16
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/google/uuid v1.2.0
 	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
-	github.com/onsi/ginkgo v1.16.1 // indirect
-	github.com/onsi/gomega v1.11.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
+	go.mongodb.org/mongo-driver v1.5.1
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
+	golang.org/x/sys v0.0.0-20210112080510-489259a85091 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,30 +1,33 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/go-redis/redis"
+	"context"
+	"net/http"
+	"os"
+
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"net/http"
-	"os"
-	"regexp"
-	"sync"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+var (
+	_ = godotenv.Load()
+	ctx = context.Background()
+	mongoClient, _ = mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("DB_HOST")))
+	mongoDatabase = mongoClient.Database(os.Getenv("USER_DB"))
+	mongoCollection = mongoDatabase.Collection(os.Getenv("USER_COLLECTION"))
 )
 
 type User struct {
-	Name        string `json:"name"`
-	PhoneNumber string `json:"phone_number"`
-	ImageURL    string `json:"image_url"`
-	Status      string `json:"status"`
-	UUID        string `json:"uuid"`
+	Name        string `json:"name" bson:"name"`
+	PhoneNumber string `json:"phone_number" bson:"phone_number"`
+	ImageURL    string `json:"image_url" bson:"image_url"`
+	Status      string `json:"status" bson:"status"`
+	UUID        string `json:"uuid" bson:"_id"`
 }
-
-var _ = godotenv.Load()
-var opt, _ = redis.ParseURL(os.Getenv("REDIS_HOST"))
-var redisClient = redis.NewClient(opt)
 
 func main() {
 	instance := echo.New()
@@ -36,11 +39,9 @@ func main() {
 	instance.POST("/users/update", updateProfile)
 
 	instance.GET("/users/profile/:id", getProfile)
-	instance.GET("/users/get-all/id", getAllIds)
 	instance.GET("/users/get-all/profile", getAllProfiles)
 
 	instance.GET("/users/delete/:id", deleteProfile)
-	instance.GET("/users/flush", flushAll)
 
 	instance.Logger.Fatal(instance.Start(":1001"))
 }
@@ -52,7 +53,29 @@ func saveProfile(cnt echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	if user.UUID == "" {
+	if user.PhoneNumber == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, false)
+	}
+
+	userByPhone := getUserByPhoneNumber(user.PhoneNumber)
+
+	if userByPhone.UUID != "" {
+		user.UUID = userByPhone.UUID
+		bsonUser, err := bson.Marshal(&user)
+		bson.Unmarshal(bsonUser, &user)
+
+		if err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+
+		mongoCollection.UpdateByID(ctx, userByPhone.UUID, bson.M{
+			"$set": user,
+		})
+
+		return cnt.JSON(http.StatusOK, user)
+	}
+
+	if  user.UUID == "" {
 		user.UUID = uuid.NewString()
 	} else {
 		_, err := uuid.Parse(user.UUID)
@@ -62,63 +85,42 @@ func saveProfile(cnt echo.Context) error {
 		}
 	}
 
-	jsonValue, err := json.Marshal(user)
+	userByUUID := getUserByUUID(user.UUID)
 
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	if userByUUID.PhoneNumber != "" {
+		user.PhoneNumber = userByUUID.PhoneNumber
 	}
 
-	redisClient.Set(generateUserRedisKey(user.UUID), jsonValue, 0)
-
-	return cnt.JSON(http.StatusCreated, user)
-}
-
-func getProfile(cnt echo.Context) error {
-	user := new(User)
-	userId := cnt.Param("id")
-
-	_, err := uuid.Parse(userId)
-
-	if err != nil {
-		return echo.NewHTTPError(http.StatusNoContent, err.Error())
-	}
-
-	userData, _ := redisClient.Get(generateUserRedisKey(userId)).Bytes()
-
-	_ = json.Unmarshal(userData, &user)
+	mongoCollection.InsertOne(ctx, user)
 
 	return cnt.JSON(http.StatusOK, user)
 }
 
-func getAllIds(cnt echo.Context) error {
-	users := redisClient.Keys(generateUserRedisKey("*")).Val()
+func getProfile(cnt echo.Context) error {
+	userId := cnt.Param("id")
+	_, err := uuid.Parse(userId)
 
-	return cnt.JSON(http.StatusOK, map[string]interface{}{
-		"users": users,
-		"count": len(users),
-	})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusOK, err.Error())
+	}
+	
+	return cnt.JSON(http.StatusOK, getUserByUUID(userId))
 }
 
 func getAllProfiles(cnt echo.Context) error {
-	users := make([]interface{}, 0)
+	users := make([]*User, 0)
 	user := new(User)
-	iter := redisClient.Keys(generateUserRedisKey("*")).Val()
 
-	wg := new(sync.WaitGroup)
-	wg.Add(len(iter))
+	userList, err := mongoCollection.Find(ctx, bson.D{})
 
-	for index := range iter {
-		go func(userIndex int) {
-			defer func() {
-				wg.Done()
-			}()
-			userData, _ := redisClient.Get(generateUserRedisKey(iter[userIndex])).Bytes()
-			_ = json.Unmarshal(userData, &user)
-			users = append(users, user)
-		}(index)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	} 
+
+	for userList.Next(ctx) {
+		userList.Decode(&user)
+		users = append(users, user)
 	}
-
-	wg.Wait()
 
 	return cnt.JSON(http.StatusOK, map[string]interface{}{
 		"users": users,
@@ -133,63 +135,65 @@ func updateProfile(cnt echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	userRedisKey := generateUserRedisKey(user.UUID)
-	uuidQuery := redisClient.Keys(userRedisKey).Val()
-
-	if len(uuidQuery) > 0 && uuidQuery[0] == userRedisKey {
-		jsonValue, err := json.Marshal(user)
-
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-		}
-
-		redisClient.Set(userRedisKey, jsonValue, 0)
-
-		return cnt.JSON(http.StatusAccepted, map[string]bool{
-			"status": true,
-		})
+	if user.UUID == "" && user.PhoneNumber == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, false)
 	}
 
-	return cnt.JSON(http.StatusAccepted, map[string]bool{
-		"status": false,
-	})
-}
+	existingUserByPhoneNumber := getUserByPhoneNumber(user.PhoneNumber)
+	existingUserByUUID := getUserByUUID(user.UUID)
+	
+	if existingUserByPhoneNumber.PhoneNumber == "" && existingUserByUUID.UUID == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, false)
+	}
 
-func deleteProfile(cnt echo.Context) error {
-	userId := cnt.Param("id")
+	if existingUserByUUID.UUID != "" {
+		user.PhoneNumber = existingUserByUUID.PhoneNumber
+	} else if existingUserByPhoneNumber.PhoneNumber != "" {
+		user.UUID = existingUserByPhoneNumber.UUID
+	}
 
-	redisClient.Del(generateUserRedisKey(userId))
-
-	return cnt.JSON(http.StatusResetContent, true)
-}
-
-func flushAll(cnt echo.Context) error {
-	redisClient.FlushAll()
-
-	return cnt.JSON(http.StatusNoContent, map[string]bool{
-		"status": true,
-	})
-}
-
-/*func getUserByPhoneNumber(string phoneNumber) error {
+	bsonUser, err := bson.Marshal(&user)
+	bson.Unmarshal(bsonUser, &user)
 
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	return cnt.JSON(http.StatusOK, userUUID) //get sql here
-}*/
+	mongoCollection.UpdateByID(ctx, user.UUID, bson.M{
+		"$set": user,
+	})
 
-func generateUserRedisKey(userId string) string {
-	res, err := regexp.MatchString(`^user-`, userId)
+	return cnt.JSON(http.StatusOK, user)
+}
 
-	if err != nil {
-		return ""
+func deleteProfile(cnt echo.Context) error {
+	userId := cnt.Param("id")
+
+	_, err := mongoCollection.DeleteOne(ctx, bson.M{"_id": userId})
+
+	return cnt.JSON(http.StatusOK, err)
+}
+
+func getUserByPhoneNumber(phoneNumber string) *User {
+	user := new(User)
+	
+	if phoneNumber == "" {
+		return user
 	}
 
-	if res == true {
-		return userId
-	}
+	mongoCollection.FindOne(ctx, bson.M{"phone_number": phoneNumber}).Decode(&user)
 
-	return fmt.Sprint("user-", userId)
+	return user
+}
+
+func getUserByUUID(uuid string) *User {
+	user := new(User)
+
+	if uuid == "" {
+		return user
+	}
+	
+	mongoCollection.FindOne(ctx, bson.M{"_id": uuid}).Decode(&user)
+
+	return user
 }
